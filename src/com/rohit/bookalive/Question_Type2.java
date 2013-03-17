599c4c4dce0dc168a8cbf0ed30e6efdc85b95358
R G <rohit@iiith>
2013-03-17 15:05:15 +0530

    qtype 2 ok except poly int

---
+++ src/com/rohit/bookalive/Question_Type2.java
@@ -0,0 +1,97 @@
+package com.rohit.bookalive;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.opencv.core.Mat;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import android.util.Log;
+
+public class Question_Type2 extends Question {
+	RegionOfInterest roi;
+	private boolean done = false;
+	private final static String TAG = "Question_Type2";
+	
+	private final int MAX_POINTS = 4;
+	private double[] px = new double[MAX_POINTS], py = new double[MAX_POINTS];
+	private int gotPoints = 0;
+	
+	@Override
+	public void read() {
+		try {
+			File f = new File(qfname);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(f);
+			doc.getDocumentElement().normalize();
+
+			Log.v(TAG,doc.getDocumentElement().getNodeName());
+			Element objects = (Element) doc.getElementsByTagName("objects").item(0);
+			NodeList ptList = objects.getElementsByTagName("roi").item(0).getChildNodes();
+			int plen = 0;
+			for(int i=0; i<ptList.getLength(); i++) {
+				Node pt = ptList.item(i);
+				if(pt.getNodeType() == Node.ELEMENT_NODE) {
+					Element pte = (Element) pt;
+					double x = Double.parseDouble(pte.getElementsByTagName("x").item(0).getTextContent());
+					double y = Double.parseDouble(pte.getElementsByTagName("y").item(0).getTextContent());
+					px[plen] = x; py[plen] = y; plen++;
+				}
+			}
+			Polygon P = new Polygon(px,py,plen);
+			roi = new RegionOfInterest("Pickup", P);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void draw() {
+		
+		/*
+		// TODO remove this, temp for now, key location
+		
+			capImg.drawLine(roi.P.polyX[0], roi.P.polyY[0], roi.P.polyX[1], roi.P.polyY[1]);
+			capImg.drawLine(roi.P.polyX[1], roi.P.polyY[1], roi.P.polyX[2], roi.P.polyY[2]);
+			capImg.drawLine(roi.P.polyX[2], roi.P.polyY[2], roi.P.polyX[3], roi.P.polyY[3]);
+			capImg.drawLine(roi.P.polyX[3], roi.P.polyY[3], roi.P.polyX[0], roi.P.polyY[0]);
+		*/
+		
+		// Draw between last 2 points
+		if(gotPoints < 2) return;
+		capImg.drawLine(px[gotPoints-1], py[gotPoints-1], px[gotPoints-2], py[gotPoints-2]);
+		if(gotPoints == MAX_POINTS) {
+			capImg.drawLine(px[gotPoints-1], py[gotPoints-1], px[0], py[0]);
+		}
+	}
+
+	@Override
+	public void clicked(double x, double y) {
+		if(gotPoints == 0) capImg.revertImage();
+		px[gotPoints] = x; py[gotPoints] = y;
+		gotPoints ++;
+		draw();
+		if(gotPoints == MAX_POINTS) {
+			Polygon test = new Polygon(px, py, gotPoints);
+			if(roi.P.overlap(test) > 0.7) {
+				done = true;
+				show("CORRECT", 1000);
+			} else {
+				show("NO! Try again",1000);
+			}
+			gotPoints = 0;
+		}
+	}
+
+	@Override
+	public boolean checkDone() {
+		return done;
+	}
+}
