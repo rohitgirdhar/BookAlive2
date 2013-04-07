2a4afbb8aefc57f589495513904971903c05d7ec
R G <rohit@iiith>
2013-04-08 02:39:22 +0530

    supporting onclick animations, with sounds

--- src/com/rohit/bookalive/Anime.java
+++ src/com/rohit/bookalive/Anime.java
@@ -1,20 +1,29 @@
 package com.rohit.bookalive;
 
+import java.util.Locale;
+
 import org.opencv.core.Point;
 import org.w3c.dom.Element;
 
 import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class Anime {
+	private static final String TAG = "Anime";
 	public String type;
 	public double tlx, tly;
 	public double wd, ht;
+	public boolean onclick = false;
 	private RelativeLayout relLayout;
 	private Context context;
+	private AnimationDrawable frameAnime;
+	RelativeLayout.LayoutParams animParams;
+	ImageView animeView;
 	
 	public Anime(RelativeLayout layout, Context context, Element anim) {
 		relLayout = layout;
@@ -24,28 +33,50 @@ public class Anime {
 		tly = Double.parseDouble(anim.getAttribute("tly"));
 		wd = Double.parseDouble(anim.getAttribute("width"));
 		ht = Double.parseDouble(anim.getAttribute("height"));
+		if(anim.getAttribute("onclick").toLowerCase(Locale.ENGLISH).equals("true")) onclick = true;
+		initView();
 	}
 	
-	public void animate() {
-		ImageView anime = new ImageView(context);
-		anime.setVisibility(View.VISIBLE);
-		if(type.equals("fire")) {
-			anime.setBackgroundResource(R.drawable.anime_fire);
-		} else if(type.equals("explode")) {
-			anime.setBackgroundResource(R.drawable.anime_explode);
-		} else if(type.equals("smoke")) {
-			anime.setBackgroundResource(R.drawable.anime_smoke);
-		} else {
-			throw new IllegalArgumentException("Invalid Anime Type : " + type);
-		}
-		AnimationDrawable frameAnime = (AnimationDrawable) anime.getBackground();
-		RelativeLayout.LayoutParams animParams = new RelativeLayout.LayoutParams((int)wd, (int)ht);
+	private void initView() {
+		animParams = new RelativeLayout.LayoutParams((int)wd, (int)ht);
 		Point p = new Point(tlx, tly);
 		Util.getPointOnOrig(CapturedImage.H.inv(), p);
 		animParams.topMargin = (int) p.y;
 		animParams.leftMargin = (int) p.x;
-		anime.setLayoutParams(animParams);	
-		relLayout.addView(anime);
+	}
+	
+	private void initAnimate() {
+		animeView = new ImageView(context);
+		animeView.setVisibility(View.VISIBLE);
+		
+		animeView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//Log.v(TAG,"clicked");
+				if(onclick) animate();
+			}
+		});
+		if(type.equals("fire")) {
+			animeView.setBackgroundResource(R.drawable.anime_fire);
+		} else if(type.equals("explode")) {
+			animeView.setBackgroundResource(R.drawable.anime_explode);
+		} else if(type.equals("smoke")) {
+			animeView.setBackgroundResource(R.drawable.anime_smoke);
+		} else {
+			throw new IllegalArgumentException("Invalid Anime Type : " + type);
+		}
+		frameAnime = (AnimationDrawable) animeView.getBackground();
+		animeView.setLayoutParams(animParams);
+		relLayout.addView(animeView);
+	}
+	
+	public void animate() {
+		initAnimate();	
+		if(onclick) frameAnime.setOneShot(true);
 		frameAnime.start();
+		if(onclick) {
+			MediaPlayer mp = MediaPlayer.create(context, R.raw.explode);
+			mp.start();
+		}	
 	}
 }
